28ae37dd0e3d25d9709c2a4385449be0c073744f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-29 19:25:30 -0300

    feat(helpers/genericValidations): create validation functions
    - functions execute simple and generic validations

---
+++ src/helpers/genericValidations/index.ts
@@ -0,0 +1,43 @@
+const isNull = (...value: any[]) => value.every((v) => v === null);
+
+const isDefinied = (...value: any[]) =>
+  value.every((v) => typeof v !== 'undefined');
+
+const isNotEmpty = (...value: string[] | any[]) =>
+  value.every((v: string | any[]) => v.length > 0);
+
+const isMoreThenMinLength = (minLength: number, ...strings: string[]) =>
+  strings.every((s) => s.length >= minLength);
+
+const isBetweenMaxAndMinLength = (
+  maxLength: number,
+  minLength: number,
+  ...strings: string[]
+) => strings.every((s) => s.length <= maxLength && s.length >= minLength);
+
+const isMoreThenMinNumber = (minNumber: number, ...numbers: number[]) =>
+  numbers.every((n) => n >= minNumber);
+
+const isLessThenMaxNumber = (maxNumber: number, ...numbers: number[]) =>
+  numbers.every((n) => n <= maxNumber);
+
+const isBetweenMaxAndMinNumber = (
+  maxNumber: number,
+  minNumber: number,
+  ...numbers: number[]
+) => numbers.every((n) => n <= maxNumber && n >= minNumber);
+
+const haveFormat = (regexFormat: RegExp, ...strings: string[]) =>
+  strings.every((s) => regexFormat.test(s));
+
+export {
+  isNull,
+  isDefinied,
+  isNotEmpty,
+  isMoreThenMinLength,
+  isBetweenMaxAndMinLength,
+  isMoreThenMinNumber,
+  isLessThenMaxNumber,
+  isBetweenMaxAndMinNumber,
+  haveFormat,
+};
