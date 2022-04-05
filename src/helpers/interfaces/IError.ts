50ab6acae9e33b8b6bddd403b9224bbfb7af4e2c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-05 18:39:27 -0300

    feat(helpers/interfaces): create Error Interface

---
+++ src/helpers/interfaces/IError.ts
@@ -0,0 +1,16 @@
+type ErrorStatusCode = 
+  'INTERNAL_SERVER_ERROR'
+  | 'BAD_REQUEST'
+  | 'UNAUTHORIZED'
+  | 'FORBIDDEN'
+  | 'NOT_FOUND'
+  | 'CONFLICT'
+  | 'UNPROCESSABLE_ENTITY'
+  | 'INTERNAL_SERVER_ERROR';
+
+interface IError {
+  statusCode: ErrorStatusCode;
+  message: string;
+}
+
+export { IError };
---
+++ src/helpers/interfaces/index.ts
@@ -0,0 +1,3 @@
+import { IError } from './IError';
+
+export { IError };
