5cac976afafb7bdd21da96ab8814738eb7c90b5b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-11 19:36:05 -0300

    test(unit/CreateUserController): test create user success
    - need to refactor CreateUserController to only handle success
    - need to create Controller to verify is user is unique

---
+++ src/tests/unit/user/create/CreateUserController.spec.ts
@@ -0,0 +1,68 @@
+import { NextFunction, request, response } from 'express';
+import { User } from '@prisma/client';
+import { expect } from 'chai';
+import Sinon from 'sinon';
+
+import { ISuccess } from '../../../../helpers/interfaces';
+
+import { UserRepository } from '../../../../modules/users/repository/UsersRepository';
+import { CreateUserUseCase } from '../../../../modules/users/useCases/createUser/CreateUserUseCase';
+import { CreateUserController } from '../../../../modules/users/useCases/createUser/CreateUserController';
+
+const NEW_USER: User = {
+  id: '5',
+  email: 'person5@email.com',
+  username: 'person5',
+  password: '123456',
+};
+
+const userRepository = new UserRepository();
+const createUserUseCase = new CreateUserUseCase(userRepository);
+const createUserController = new CreateUserController(createUserUseCase);
+
+describe('Test CreateUserController', () => {
+  let spiedResponse: Sinon.SinonSpy;
+  let spiedJson: Sinon.SinonSpy;
+  let useCaseStub: Sinon.SinonStub;
+  const next: NextFunction = () => {};
+
+  before(() => {
+    spiedResponse = Sinon.spy(response, 'status');
+    spiedJson = Sinon.spy(response, 'json');
+  });
+
+  describe('Success case', () => {
+    const SUCCES_RESPONSE: ISuccess<User> = {
+      statusCode: 'CREATED',
+      data: NEW_USER,
+    };
+
+    before(() => {
+      useCaseStub = Sinon.stub(createUserUseCase, 'execute').resolves(
+        SUCCES_RESPONSE
+      );
+
+      request.body = {
+        email: NEW_USER.email,
+        username: NEW_USER.username,
+        password: NEW_USER.password,
+      };
+    });
+
+    after(() => {
+      useCaseStub.restore();
+    });
+
+    it('should return a response with status 201', async () => {
+      await createUserController.handle(request, response, next);
+
+      expect(spiedResponse.calledWith(201)).to.be.true;
+    });
+
+    it('should return a response with the user created', async () => {
+      await createUserController.handle(request, response, next);
+
+      expect(spiedJson.calledWith(SUCCES_RESPONSE.data)).to.be.true;
+    });
+  });
+});
