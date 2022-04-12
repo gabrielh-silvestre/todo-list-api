0cea8dd77c61e43c08afcc2ce814dafdcf5c95d4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-12 18:14:16 -0300

    feat(prisma/seed): add tasks to seed db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,25 @@
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  const firstUserId = uuid();
+  const secondUserId = uuid();
+
+  await prisma.user.deleteMany({});
+  await prisma.task.deleteMany({});
+
   await prisma.user.createMany({
     data: [
       {
+        id: firstUserId,
         email: 'firstPerson@gmail.com',
         username: 'firstPerson',
         password: bcrypt.hashSync('123a456', 10),
       },
       {
+        id: secondUserId,
         email: 'secondPerson@gmail.com',
         username: 'secondPerson',
         password: bcrypt.hashSync('123b456', 10),
@@ -19,6 +28,26 @@ async function main() {
         email: 'thirdPerson@gmail.com',
         username: 'thirdPerson',
         password: bcrypt.hashSync('123c456', 10),
+      },
+    ],
+  });
+
+  await prisma.task.createMany({
+    data: [
+      {
+        title: 'First task',
+        description: 'First task description',
+        user_id: firstUserId,
+      },
+      {
+        title: 'Second task',
+        description: 'Second task description',
+        user_id: firstUserId,
+      },
+      {
+        title: 'Third task',
+        description: 'Third task description',
+        user_id: secondUserId,
       },
     ],
   });
