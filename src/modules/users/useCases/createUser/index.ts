d1f2e4fb47b9b8567efd37e48478a2a93c8eee22
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-12 00:11:31 -0300

    fix(users/useCases/createUser): inject auth service in CreateUserUseCase

--- src/modules/users/useCases/createUser/index.ts
+++ src/modules/users/useCases/createUser/index.ts
@@ -1,9 +1,11 @@
+import { Auth } from '../../../../midleware/auth';
 import { UserRepository } from '../../repository/UsersRepository';
 import { CreateUserUseCase } from './CreateUserUseCase';
 import { CreateUserController } from './CreateUserController';
 
+const auth = new Auth();
 const userRepository = new UserRepository();
-const createUserUseCase = new CreateUserUseCase(userRepository);
+const createUserUseCase = new CreateUserUseCase(userRepository, auth);
 const createUserController = new CreateUserController(createUserUseCase);
 
 export { createUserController };
