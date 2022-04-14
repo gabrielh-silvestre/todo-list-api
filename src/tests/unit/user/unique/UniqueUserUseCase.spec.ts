a7531e58b8cbc5da65eba1ca3da3ccdb32c100ed
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-14 00:16:20 -0300

    test(unit/user/unique): change behavior of UniqueUserUseCase
    - throws errors if fails instead of returning IErrors objects
    - add test for database errors

--- src/tests/unit/user/unique/UniqueUserUseCase.spec.ts
+++ src/tests/unit/user/unique/UniqueUserUseCase.spec.ts
@@ -2,10 +2,12 @@ import { User } from '@prisma/client';
 import { expect } from 'chai';
 import Sinon from 'sinon';
 
-import { IError, ISuccess } from '../../../../@types/interfaces';
+import { ISuccess } from '../../../../@types/interfaces';
 
 import { UserRepository } from '../../../../modules/users/repository/UsersRepository';
 import { UniqueUserUseCase } from '../../../../modules/users/useCases/uniqueUser/UniqueUserUseCase';
+
+import { CustomError } from '../../../../utils/CustomError';
 
 const NEW_USER: User = {
   id: '1',
@@ -17,7 +19,7 @@ const NEW_USER: User = {
 const userRepository = new UserRepository();
 const uniqueUserUseCase = new UniqueUserUseCase(userRepository);
 
-describe('Test UniqueUserUseCase', () => {
+describe.only('Test UniqueUserUseCase', () => {
   let findByEmailStub: Sinon.SinonStub;
 
   describe('Success case', () => {
@@ -61,19 +63,63 @@ describe('Test UniqueUserUseCase', () => {
       findByEmailStub.restore();
     });
 
-    describe('Should return a object with an error status and message', () => {
+    describe('Should throw a stringfy error with status and message', () => {
       const { email } = NEW_USER;
 
-      it('success status should be "CONFLICT"', async () => {
-        const response = await uniqueUserUseCase.execute(email);
-
-        expect(response.statusCode).to.be.equal('CONFLICT');
+      it('status should be "CONFLICT"', async () => {
+        try {
+          await uniqueUserUseCase.execute(email);
+          expect.fail('Should throw an error');
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.statusCode).to.be.equal('CONFLICT');
+        }
       });
 
-      it('error message should be "User already exists"', async () => {
-        const response = (await uniqueUserUseCase.execute(email)) as IError;
+      it('message should be "User already exists"', async () => {
+        try {
+          await uniqueUserUseCase.execute(email);
+          expect.fail('Should throw an error');
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.message).to.be.equal('User already exists');
+        }
+      });
+    });
+  });
 
-        expect(response.message).to.be.equal('User already exists');
+  describe('Database error case', () => {
+    before(() => {
+      findByEmailStub = Sinon.stub(userRepository, 'findByEmail').rejects();
+    });
+
+    after(() => {
+      findByEmailStub.restore();
+    });
+
+    describe('Should throw a stringfy error with status and message', () => {
+      const { email } = NEW_USER;
+
+      it('status should be "INTERNAL_SERVER_ERROR"', async () => {
+        try {
+          await uniqueUserUseCase.execute(email);
+          expect.fail('Should throw an error');
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.statusCode).to.be.equal('INTERNAL_SERVER_ERROR');
+        }
+      });
+
+      it('message should be "Unexpected error while checking user uniqueness"', async () => {
+        try {
+          await uniqueUserUseCase.execute(email);
+          expect.fail('Should throw an error');
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.message).to.be.equal(
+            'Unexpected error while checking user uniqueness'
+          );
+        }
       });
     });
   });
