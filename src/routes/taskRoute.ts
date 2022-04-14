448e730e94d9628a6b6c86cbbbeb20972b2b303d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-13 22:49:27 -0300

    feat(routes/taskRoute): create delete task endpoint
    - verify if task exists before delete

--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import { authMiddleware } from '../midleware/auth';
+import { taskValidator } from '../midleware/validate/TaskValidator';
+
 import { uniqueTaskController } from '../modules/tasks/useCases/uniqueTask';
 import { createTaskController } from '../modules/tasks/useCases/createTask';
-import { taskValidator } from '../midleware/validate/TaskValidator';
+import { verifyTaskController } from '../modules/tasks/useCases/verifyTask';
+import { deleteTaskController } from '../modules/tasks/useCases/deleteTask';
 
 const taskRouter = express.Router();
 
@@ -14,6 +17,12 @@ taskRouter.post(
   taskValidator.createValidation,
   uniqueTaskController.handle,
   createTaskController.handle
+);
+
+taskRouter.delete(
+  '/',
+  verifyTaskController.handle,
+  deleteTaskController.handle
 );
 
 export { taskRouter };
