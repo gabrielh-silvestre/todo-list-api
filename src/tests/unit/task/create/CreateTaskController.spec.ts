d91d582868c3b175e4ad4da145cad570229bb40b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-15 00:17:36 -0300

    test(unit/task/create): change behavior of CreateTaskController
    - call next error middleware for any error

--- src/tests/unit/task/create/CreateTaskController.spec.ts
+++ src/tests/unit/task/create/CreateTaskController.spec.ts
@@ -7,8 +7,9 @@ import { TasksRepository } from '../../../../modules/tasks/repository/TasksRepos
 import { CreateTaskUseCase } from '../../../../modules/tasks/useCases/createTask/CreateTaskUseCase';
 import { CreateTaskController } from '../../../../modules/tasks/useCases/createTask/CreateTaskController';
 import { ISuccess } from '../../../../@types/interfaces';
+import { CustomError } from '../../../../utils/CustomError';
 
-const NEW_TASK: Task = {
+const MOCK_TASK: Task = {
   id: '5',
   title: 'Task 5',
   description: 'Description 5',
@@ -25,24 +26,29 @@ describe('Test CreateTaskController', () => {
   let useCaseStub: Sinon.SinonStub;
   let spiedStatus: Sinon.SinonSpy;
   let spiedJson: Sinon.SinonSpy;
-  const next: NextFunction = () => {};
+  let spiedNext: Sinon.SinonSpy;
+  const next = {
+    next: (args) => {},
+  } as { next: NextFunction };
 
   before(() => {
     spiedStatus = Sinon.spy(response, 'status');
     spiedJson = Sinon.spy(response, 'json');
+    spiedNext = Sinon.spy(next, 'next');
   });
 
   after(() => {
     spiedStatus.restore();
     spiedJson.restore();
+    spiedNext.restore();
   });
 
   describe('Success case', () => {
-    const { title, description, userId } = NEW_TASK;
+    const { title, description, userId } = MOCK_TASK;
 
     const SUCCES_RESPONSE: ISuccess<Task> = {
       statusCode: 'CREATED',
-      data: NEW_TASK,
+      data: MOCK_TASK,
     };
 
     before(() => {
@@ -64,15 +70,48 @@ describe('Test CreateTaskController', () => {
 
     describe('Should return a object with an success status and data', () => {
       it('success status should be 201', async () => {
-        await createTaskController.handle(request, response, next);
+        await createTaskController.handle(request, response, next.next);
 
         expect(spiedStatus.calledWith(201)).to.be.true;
       });
 
       it('data should be the created Task', async () => {
-        await createTaskController.handle(request, response, next);
+        await createTaskController.handle(request, response, next.next);
 
-        expect(spiedJson.calledWith(NEW_TASK)).to.be.true;
+        expect(spiedJson.calledWith(MOCK_TASK)).to.be.true;
+      });
+    });
+  });
+
+  describe('Error case', () => {
+    const { title, description, userId } = MOCK_TASK;
+    const ERROR_RESPONSE = new CustomError(
+      'INTERNAL_SERVER_ERROR',
+      'Unexpected error while creating task'
+    );
+
+    before(() => {
+      useCaseStub = Sinon.stub(createTaskUseCase, 'execute').rejects(
+        ERROR_RESPONSE
+      );
+
+      request.body = {
+        title,
+        description,
+        userId,
+      };
+    });
+
+    after(() => {
+      useCaseStub.restore();
+      request.body = {};
+    });
+
+    describe('Should call "next" error middleware', () => {
+      it('"next" should be called with CustomError as args', async () => {
+        await createTaskController.handle(request, response, next.next);
+
+        expect(spiedNext.calledWith(ERROR_RESPONSE)).to.be.true;
       });
     });
   });
