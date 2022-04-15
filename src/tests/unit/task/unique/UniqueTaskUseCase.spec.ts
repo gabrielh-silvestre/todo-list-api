b086cd22bcfd03bd15f097ae9f0bceb1897f2ed4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-14 23:31:43 -0300

    test(unit/task/unique): change behavior of UniqueTaskUseCase
    - throws errors if fails instead of returning IErrors objects
    - add test for database errors

--- src/tests/unit/task/unique/UniqueTaskUseCase.spec.ts
+++ src/tests/unit/task/unique/UniqueTaskUseCase.spec.ts
@@ -1,10 +1,12 @@
 import { Task } from '@prisma/client';
 import { expect } from 'chai';
 import Sinon from 'sinon';
-import { IError, ISuccess } from '../../../../@types/interfaces';
+import { ISuccess } from '../../../../@types/interfaces';
 
 import { TasksRepository } from '../../../../modules/tasks/repository/TasksRepository';
 import { UniqueTaskUseCase } from '../../../../modules/tasks/useCases/uniqueTask/UniqueTaskUseCase';
+
+import { CustomError } from '../../../../utils/CustomError';
 
 const MOCK_TASK: Task = {
   id: '5',
@@ -18,7 +20,7 @@ const MOCK_TASK: Task = {
 const tasksRepository = new TasksRepository();
 const uniqueTaskUseCase = new UniqueTaskUseCase(tasksRepository);
 
-describe('Test UniqueTaskUseCase', () => {
+describe.only('Test UniqueTaskUseCase', () => {
   let findByTitleStub: Sinon.SinonStub;
 
   describe('Success case', () => {
@@ -31,19 +33,21 @@ describe('Test UniqueTaskUseCase', () => {
     });
 
     describe('Should return a object with an success status and data', () => {
+      const { userId, title } = MOCK_TASK;
+
       it('success status should be "OK"', async () => {
-        const response = await uniqueTaskUseCase.execute(
+        const response = (await uniqueTaskUseCase.execute(
           MOCK_TASK.userId,
           MOCK_TASK.title
-        );
+        )) as ISuccess<null>;
 
         expect(response.statusCode).to.be.equal('OK');
       });
 
       it('data should be null', async () => {
         const response = (await uniqueTaskUseCase.execute(
-          MOCK_TASK.userId,
-          MOCK_TASK.title
+          userId,
+          title
         )) as ISuccess<null>;
 
         expect(response.data).to.be.null;
@@ -62,23 +66,63 @@ describe('Test UniqueTaskUseCase', () => {
       findByTitleStub.restore();
     });
 
-    describe('Should return a object with an error status and data', () => {
-      it('success status should be "CONFLICT"', async () => {
-        const response = await uniqueTaskUseCase.execute(
-          MOCK_TASK.userId,
-          MOCK_TASK.title
-        );
+    describe('Should throw a CustomError with status and message', () => {
+      const { userId, title } = MOCK_TASK;
 
-        expect(response.statusCode).to.be.equal('CONFLICT');
+      it('status should be "CONFLICT"', async () => {
+        try {
+          await uniqueTaskUseCase.execute(userId, title);
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.statusCode).to.be.equal('CONFLICT');
+        }
       });
 
-      it('error message should be "Task already exists"', async () => {
-        const response = (await uniqueTaskUseCase.execute(
-          MOCK_TASK.userId,
-          MOCK_TASK.title
-        )) as IError;
+      it('message should be "Task with this title already exists"', async () => {
+        try {
+          await uniqueTaskUseCase.execute(userId, title);
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.message).to.be.equal(
+            'Task with this title already exists'
+          );
+        }
+      });
+    });
+  });
 
-        expect(response.message).to.be.equal('Task already exists');
+  describe('Database error case', () => {
+    before(() => {
+      findByTitleStub = Sinon.stub(tasksRepository, 'findByTitle').rejects();
+    });
+
+    after(() => {
+      findByTitleStub.restore();
+    });
+
+    describe('Should throw a CustomError with status and message', () => {
+      const { userId, title } = MOCK_TASK;
+
+      it('status should be "INTERNAL_SERVER_ERROR"', async () => {
+        try {
+          await uniqueTaskUseCase.execute(userId, title);
+          expect.fail('Should throw an error');
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.statusCode).to.be.equal('INTERNAL_SERVER_ERROR');
+        }
+      });
+
+      it('message should be "Unexpected error while checking task uniqueness"', async () => {
+        try {
+          await uniqueTaskUseCase.execute(userId, title);
+          expect.fail('Should throw an error');
+        } catch (err) {
+          const tErr = err as CustomError;
+          expect(tErr.message).to.be.equal(
+            'Unexpected error while checking task uniqueness'
+          );
+        }
       });
     });
   });
