cb3a6d49fa0e82f5110aa56ec05cd36b09bd064a
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-15 16:29:33 -0300

    feat(routes/taskRoute): create update task endpoint
    - create PUT '/:id' endpoint
    - validate update task data
    - verify if task exists before update

--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -7,13 +7,14 @@ import { uniqueTaskController } from '../modules/tasks/useCases/uniqueTask';
 import { createTaskController } from '../modules/tasks/useCases/createTask';
 import { verifyTaskController } from '../modules/tasks/useCases/verifyTask';
 import { deleteTaskController } from '../modules/tasks/useCases/deleteTask';
+import { updateTaskController } from '../modules/tasks/useCases/updateTask';
 
 const taskRouter = express.Router();
 
 taskRouter.use(authMiddleware.handle);
 
 taskRouter.post(
-  '/create',
+  '/',
   taskValidator.createValidation,
   uniqueTaskController.handle,
   createTaskController.handle
@@ -23,6 +24,13 @@ taskRouter.delete(
   '/:id',
   verifyTaskController.handle,
   deleteTaskController.handle
+);
+
+taskRouter.put(
+  '/:id',
+  taskValidator.updateValidation,
+  verifyTaskController.handle,
+  updateTaskController.handle
 );
 
 export { taskRouter };
