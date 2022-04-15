4ef91c0280d7da91d0a4a04a736c84771084155d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-15 16:28:41 -0300

    feat(middleware/validate/TaskValidator): add validation for task update
    - all fields are required, but description can be null

--- src/middleware/validate/TaskValidator.ts
+++ src/middleware/validate/TaskValidator.ts
@@ -7,6 +7,7 @@ import { CustomError } from '../../utils/CustomError';
 
 class TaskValidator implements ITaskValidator {
   private createTaskSchema: Joi.ObjectSchema;
+  private updateTaskSchema: Joi.ObjectSchema;
 
   constructor() {
     this.createTaskSchema = Joi.object({
@@ -14,10 +15,28 @@ class TaskValidator implements ITaskValidator {
       description: Joi.string().max(120),
       userId: Joi.string().required(),
     });
+
+    this.updateTaskSchema = Joi.object({
+      title: Joi.string().min(5).max(20).required(),
+      description: Joi.string().max(120).allow(null).required(),
+      status: Joi.string().valid('TODO', 'IN_PROGRESS', 'DONE').required(),
+      userId: Joi.string().required(),
+    });
   }
 
-  createValidation = (req: Request, res: Response, next: NextFunction) => {
+  createValidation = (req: Request, _res: Response, next: NextFunction) => {
     const { error } = this.createTaskSchema.validate(req.body);
+
+    if (error) {
+      const err = new CustomError('BAD_REQUEST', error.details[0].message);
+      return next(err);
+    }
+
+    next();
+  };
+
+  updateValidation = (req: Request, _res: Response, next: NextFunction) => {
+    const { error } = this.updateTaskSchema.validate(req.body);
 
     if (error) {
       const err = new CustomError('BAD_REQUEST', error.details[0].message);
