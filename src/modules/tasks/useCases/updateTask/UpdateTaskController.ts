6a7aa6b06d01254c8e7eeef7abae183781f918f5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-15 15:56:09 -0300

    feat(tasks/useCases/updateTask): implement update task case
    - develop case based on unit test

---
+++ src/modules/tasks/useCases/updateTask/UpdateTaskController.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { UpdateTaskUseCase } from './UpdateTaskUseCase';
+
+import { successStatusCode } from '../../../../utils/successCode';
+
+class UpdateTaskController {
+  constructor(private updateTaskUseCase: UpdateTaskUseCase) {}
+
+  handle = async (req: Request, res: Response, next: NextFunction) => {
+    const { id, title, description, status, userId } = req.body;
+
+    try {
+      const { statusCode, data } = await this.updateTaskUseCase.execute(
+        userId,
+        id,
+        {
+          title,
+          description,
+          status,
+        }
+      );
+
+      return res.status(successStatusCode[statusCode]).json(data);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+export { UpdateTaskController };
---
+++ src/modules/tasks/useCases/updateTask/UpdateTaskUseCase.ts
@@ -0,0 +1,43 @@
+import { TaskStatus } from '@prisma/client';
+
+import { ITasksRepository } from '../../repository/ITasksRepository';
+import { ISuccess } from '../../../../@types/interfaces';
+import { TaskReturn } from '../../../../@types/types';
+
+import { CustomError } from '../../../../utils/CustomError';
+
+interface IRequest {
+  title: string;
+  description: string | null;
+  status: TaskStatus;
+}
+
+class UpdateTaskUseCase {
+  constructor(private tasksRepository: ITasksRepository) {}
+
+  async execute(
+    userId: string,
+    id: string,
+    { title, description, status }: IRequest
+  ): Promise<ISuccess<TaskReturn>> {
+    try {
+      const updatedTask = await this.tasksRepository.update(userId, id, {
+        title,
+        description,
+        status,
+      });
+
+      return {
+        statusCode: 'UPDATED',
+        data: updatedTask,
+      };
+    } catch (err) {
+      throw new CustomError(
+        'INTERNAL_SERVER_ERROR',
+        'Unexpected error while updating task'
+      );
+    }
+  }
+}
+
+export { UpdateTaskUseCase };
