3c5344d6083daff14c04406ca07425b1ead24dc4
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-15 00:46:05 -0300

    test(unit/task/delete): change behavior of DeleteTaskController
    - call next error middleware for any error

--- src/tests/unit/task/delete/DeleteTaskController.spec.ts
+++ src/tests/unit/task/delete/DeleteTaskController.spec.ts
@@ -7,6 +7,8 @@ import { TasksRepository } from '../../../../modules/tasks/repository/TasksRepos
 import { DeleteTaskUseCase } from '../../../../modules/tasks/useCases/deleteTask/DeleteTaskUseCase';
 import { DeleteTaskController } from '../../../../modules/tasks/useCases/deleteTask/DeleteTaskController';
 
+import { CustomError } from '../../../../utils/CustomError';
+
 const MOCK_TASK: Task = {
   id: '5',
   title: 'Task 5',
@@ -21,24 +23,29 @@ const deleteTaskUseCase = new DeleteTaskUseCase(tasksRepository);
 const deleteTaskController = new DeleteTaskController(deleteTaskUseCase);
 
 describe('Test DeleteTaskController', () => {
+  const { id, userId } = MOCK_TASK;
+
   let useCaseStub: Sinon.SinonStub;
   let spiedStatus: Sinon.SinonSpy;
   let spiedEnd: Sinon.SinonSpy;
-  const next: NextFunction = () => {};
+  let spiedNext: Sinon.SinonSpy;
+  const next = {
+    next: (args) => {},
+  } as { next: NextFunction };
 
   before(() => {
     spiedStatus = Sinon.spy(response, 'status');
     spiedEnd = Sinon.spy(response, 'end');
+    spiedNext = Sinon.spy(next, 'next');
   });
 
   after(() => {
     spiedStatus.restore();
     spiedEnd.restore();
+    spiedNext.restore();
   });
 
   describe('Success case', () => {
-    const { id } = MOCK_TASK;
-
     before(() => {
       useCaseStub = Sinon.stub(deleteTaskUseCase, 'execute').resolves({
         statusCode: 'DELETED',
@@ -50,7 +57,7 @@ describe('Test DeleteTaskController', () => {
       };
 
       request.body = {
-        userId: '1',
+        userId,
       };
     });
 
@@ -62,15 +69,50 @@ describe('Test DeleteTaskController', () => {
 
     describe('Should return a response with an success status', () => {
       it('success status should be 204', async () => {
-        await deleteTaskController.handle(request, response, next);
+        await deleteTaskController.handle(request, response, next.next);
 
         expect(spiedStatus.calledWith(204)).to.be.true;
       });
 
       it('response should end', async () => {
-        await deleteTaskController.handle(request, response, next);
+        await deleteTaskController.handle(request, response, next.next);
 
         expect(spiedEnd.calledWith()).to.be.true;
+      });
+    });
+  });
+
+  describe('Error case', () => {
+    const ERROR_RESPONSE = new CustomError(
+      'INTERNAL_SERVER_ERROR',
+      'Unexpected error while deleting task'
+    );
+
+    before(() => {
+      useCaseStub = Sinon.stub(deleteTaskUseCase, 'execute').rejects(
+        ERROR_RESPONSE
+      );
+
+      request.params = {
+        id,
+      };
+
+      request.body = {
+        userId,
+      };
+    });
+
+    after(() => {
+      useCaseStub.restore();
+      request.params = {};
+      request.body = {};
+    });
+
+    describe('Should call "next" error middleware', () => {
+      it('"next" should be called with CustomError as args', async () => {
+        await deleteTaskController.handle(request, response, next.next);
+
+        expect(spiedNext.calledWith(ERROR_RESPONSE)).to.be.true;
       });
     });
   });
