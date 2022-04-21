10416b494d2003fee113081a7f6a3382cc4431e5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-20 22:44:14 -0300

    feat(app): create route for documentation
    - use swagger to create docs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { userRouter } from './routes/userRoute';
 import { taskRouter } from './routes/taskRoute';
+
 import { errorHandler } from './middleware/error';
+
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/v1/api/users', userRouter);
 app.use('/v1/api/tasks', taskRouter);
 
