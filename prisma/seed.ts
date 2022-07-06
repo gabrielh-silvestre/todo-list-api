7021790e7ceca0a82157b4fef547058203c04b02
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-07-06 15:21:37 -0300

    style(prisma/seed): use double quotes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const firstUserId = '56fbbb34-5c6b-4237-b8ed-22412ca935b7';
-  const secondUserId = '1d457863-4c42-4d3e-a43f-82e376380070';
-  const thirdUserId = '1d457863-4c42-3d3e-a43f-82e376380070';
+  const firstUserId = "56fbbb34-5c6b-4237-b8ed-22412ca935b7";
+  const secondUserId = "1d457863-4c42-4d3e-a43f-82e376380070";
+  const thirdUserId = "1d457863-4c42-3d3e-a43f-82e376380070";
 
   await prisma.task.deleteMany({});
   await prisma.user.deleteMany({});
@@ -14,18 +14,18 @@ async function main() {
     data: [
       {
         id: firstUserId,
-        email: 'firstPerson@gmail.com',
-        username: 'person1',
+        email: "firstPerson@gmail.com",
+        username: "person1",
       },
       {
         id: secondUserId,
-        email: 'secondPerson@gmail.com',
-        username: 'person2',
+        email: "secondPerson@gmail.com",
+        username: "person2",
       },
       {
         id: thirdUserId,
-        email: 'thirdPerson@gmail.com',
-        username: 'person3',
+        email: "thirdPerson@gmail.com",
+        username: "person3",
       },
     ],
   });
@@ -33,21 +33,21 @@ async function main() {
   await prisma.task.createMany({
     data: [
       {
-        id: 'bf9640e3-d49d-497f-8ea6-91e22de81e89',
-        title: 'First task',
-        description: 'First task description',
+        id: "bf9640e3-d49d-497f-8ea6-91e22de81e89",
+        title: "First task",
+        description: "First task description",
         userId: firstUserId,
       },
       {
-        id: 'c9b8cc4f-0a0c-4ebe-a3c9-7a3b7bf3c093',
-        title: 'Second task',
-        description: 'Second task description',
+        id: "c9b8cc4f-0a0c-4ebe-a3c9-7a3b7bf3c093",
+        title: "Second task",
+        description: "Second task description",
         userId: firstUserId,
       },
       {
-        id: 'd22f8ed1-08e6-405b-9346-f3cd70a97e6e',
-        title: 'Third task',
-        description: 'Third task description',
+        id: "d22f8ed1-08e6-405b-9346-f3cd70a97e6e",
+        title: "Third task",
+        description: "Third task description",
         userId: secondUserId,
       },
     ],
