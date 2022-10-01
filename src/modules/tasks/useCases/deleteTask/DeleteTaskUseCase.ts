ab778c8e816dffcf3c23c96c1e2acb65230ab484
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-01 12:17:09 -0300

    refactor: remove validate decorator of DeleteTaskUseCase
    - create private function for validation

--- src/modules/tasks/useCases/deleteTask/DeleteTaskUseCase.ts
+++ src/modules/tasks/useCases/deleteTask/DeleteTaskUseCase.ts
@@ -1,17 +1,24 @@
 import type { ITasksRepository } from "@projectTypes/interfaces";
 import type { TaskIdentifierById, SuccessCase } from "@projectTypes/types";
 import { StatusCodes } from "http-status-codes";
-
-import { IsTaskValid, IsTaskExists } from "../../decorators";
+import { NotFoundError } from "restify-errors";
 
 class DeleteTaskUseCase {
   constructor(private tasksRepository: ITasksRepository) {}
 
-  @IsTaskValid(IsTaskExists)
+  private async taskExists(userId: string, id: string): Promise<void | never> {
+    const task = await this.tasksRepository.findById({ userId, id });
+
+    if (!task) {
+      throw new NotFoundError("Task not found");
+    }
+  }
+
   async execute({
     userId,
     id,
   }: TaskIdentifierById): Promise<SuccessCase<null> | never> {
+    await this.taskExists(userId, id);
     await this.tasksRepository.delete({ userId, id });
 
     return {
