7959ce4de55d8c2e259f960ad8a23b8e57d1edc6
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-01 12:25:45 -0300

    refactor: remove validate decorator of UpdateTaskUseCase
    - create private function for validation

--- src/modules/tasks/useCases/updateTask/UpdateTaskUseCase.ts
+++ src/modules/tasks/useCases/updateTask/UpdateTaskUseCase.ts
@@ -5,16 +5,38 @@ import type {
   SuccessCase,
 } from "@projectTypes/types";
 import { StatusCodes } from "http-status-codes";
-
-import { IsTaskValid, IsTaskExists } from "../../decorators";
+import { ConflictError, NotFoundError } from "restify-errors";
 
 class UpdateTaskUseCase {
   constructor(private tasksRepository: ITasksRepository) {}
 
-  @IsTaskValid(IsTaskExists)
+  private async taskExists(userId: string, id: string): Promise<void | never> {
+    const task = await this.tasksRepository.findById({ userId, id });
+
+    if (!task) {
+      throw new NotFoundError("Task not found");
+    }
+  }
+
+  private async isTaskUnique(
+    userId: string,
+    id: string,
+    title: string
+  ): Promise<void | never> {
+    const existingTask = await this.tasksRepository.findById({ userId, id });
+    const [task] = await this.tasksRepository.findByExactTitle({ title, userId });
+
+    if (task && task.id !== existingTask!.id) {
+      throw new ConflictError("Task with this title already exists");
+    }
+  }
+
   async execute(
     taskData: TaskUpdateAttributes
   ): Promise<SuccessCase<TaskReturn> | never> {
+    await this.taskExists(taskData.userId, taskData.id);
+    await this.isTaskUnique(taskData.userId, taskData.id, taskData.title);
+
     const updatedTask = await this.tasksRepository.update(taskData);
 
     return {
