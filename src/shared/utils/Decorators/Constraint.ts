0dc1c3bfd1832a7500ff10119700fd24ff2d9af1
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-01 12:30:01 -0300

    chore: delete Constraint abstraction

--- src/shared/utils/Decorators/Constraint.ts
+++
@@ -1,9 +0,0 @@
-import { IDecoratorConstraint } from "@projectTypes/interfaces";
-
-abstract class Constraint<R, T> implements IDecoratorConstraint<T> {
-  constructor(protected readonly repository: R) {}
-
-  abstract validate(args: T): Promise<void | never>;
-}
-
-export { Constraint };
