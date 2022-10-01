c6bf85aafa6df3f077dcee9636d93e9b3a5906eb
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-01 15:22:04 -0300

    feat(infra/task): create in memory repository for test porpouses
    - apply unit tests on it

---
+++ src/infra/task/repository/memory/Task.repository.spec.ts
@@ -0,0 +1,115 @@
+import { TaskFactory } from "@domain/task/factory/Task.factory";
+import { expect } from "chai";
+
+import { TasksRepositoryInMemory } from "./Task.repository";
+
+const FAKE_UUID = "24d5ad6d-b864-4dee-a217-2608b6706cb6";
+
+describe("Test TasksRepository in memory", () => {
+  it("should be able to list all tasks", async () => {
+    const taskRepository = new TasksRepositoryInMemory();
+    const newTask1 = TaskFactory.create("title1", FAKE_UUID);
+    const newTask2 = TaskFactory.create("title2", FAKE_UUID);
+    const newTask3 = TaskFactory.create("title3", FAKE_UUID);
+
+    await taskRepository.create(newTask1);
+    await taskRepository.create(newTask2);
+    await taskRepository.create(newTask3);
+
+    const foundTasks = await taskRepository.findAll(FAKE_UUID);
+
+    expect(foundTasks).to.be.not.null;
+    expect(foundTasks.length).to.be.equal(3);
+  });
+
+  it("should be able to find a task by id", async () => {
+    const taskRepository = new TasksRepositoryInMemory();
+    const newTask = TaskFactory.create("title", FAKE_UUID);
+
+    await taskRepository.create(newTask);
+
+    const foundTask = await taskRepository.find(FAKE_UUID, newTask.id);
+
+    expect(foundTask).to.be.not.null;
+    expect(foundTask?.id).to.be.equal(newTask.id);
+    expect(foundTask?.title).to.be.equal(newTask.title);
+    expect(foundTask?.description).to.be.equal(newTask.description);
+    expect(foundTask?.status).to.be.equal(newTask.status);
+    expect(foundTask?.userId).to.be.equal(newTask.userId);
+  });
+
+  it("should be able to find a task by title", async () => {
+    const taskRepository = new TasksRepositoryInMemory();
+    const newTask = TaskFactory.create("title4", FAKE_UUID);
+
+    await taskRepository.create(newTask);
+
+    const foundTask = await taskRepository.findByTitle(
+      FAKE_UUID,
+      newTask.title
+    );
+
+    expect(foundTask).to.be.not.null;
+    expect(foundTask?.id).to.be.equal(newTask.id);
+    expect(foundTask?.title).to.be.equal(newTask.title);
+    expect(foundTask?.description).to.be.equal(newTask.description);
+    expect(foundTask?.status).to.be.equal(newTask.status);
+    expect(foundTask?.userId).to.be.equal(newTask.userId);
+  });
+
+  it("should be able to create a new task", async () => {
+    const taskRepository = new TasksRepositoryInMemory();
+    const newTask = TaskFactory.create("title", FAKE_UUID);
+
+    await taskRepository.create(newTask);
+
+    const foundTask = await taskRepository.find(FAKE_UUID, newTask.id);
+
+    expect(foundTask).to.be.not.null;
+    expect(foundTask?.id).to.be.equal(newTask.id);
+    expect(foundTask?.title).to.be.equal(newTask.title);
+    expect(foundTask?.description).to.be.equal(newTask.description);
+    expect(foundTask?.status).to.be.equal(newTask.status);
+    expect(foundTask?.userId).to.be.equal(newTask.userId);
+  });
+
+  it("should be able to update a task", async () => {
+    const taskRepository = new TasksRepositoryInMemory();
+    const newTask = TaskFactory.create("title", FAKE_UUID);
+
+    await taskRepository.create(newTask);
+
+    const foundTask = await taskRepository.find(FAKE_UUID, newTask.id);
+
+    foundTask?.changeTitle("new title");
+    foundTask?.changeDescription("new description");
+
+    await taskRepository.update(foundTask!);
+
+    const updatedTask = await taskRepository.find(FAKE_UUID, newTask.id);
+
+    expect(updatedTask).to.be.not.null;
+    expect(updatedTask?.id).to.be.equal(foundTask?.id);
+    expect(updatedTask?.title).to.be.equal(foundTask?.title);
+    expect(updatedTask?.description).to.be.equal(foundTask?.description);
+    expect(updatedTask?.status).to.be.equal(foundTask?.status);
+    expect(updatedTask?.userId).to.be.equal(foundTask?.userId);
+  });
+
+  it("should be able to delete a task", async () => {
+    const taskRepository = new TasksRepositoryInMemory();
+    const newTask1 = TaskFactory.create("title1", FAKE_UUID);
+    const newTask2 = TaskFactory.create("title2", FAKE_UUID);
+    const newTask3 = TaskFactory.create("title3", FAKE_UUID);
+
+    await taskRepository.create(newTask1);
+    await taskRepository.create(newTask2);
+    await taskRepository.create(newTask3);
+
+    await taskRepository.delete(FAKE_UUID, newTask2.id);
+
+    const deletedTask = await taskRepository.find(FAKE_UUID, newTask2.id);
+
+    expect(deletedTask).to.be.null;
+  });
+});
