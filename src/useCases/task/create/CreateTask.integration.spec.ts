92f9b0e68cde1e80c31684e2330397b68443cfbb
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-04 10:43:56 -0300

    test(useCases): correct imports

--- src/useCases/task/create/CreateTask.integration.spec.ts
+++ src/useCases/task/create/CreateTask.integration.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 
-import { TasksRepositoryInMemory } from "@infra/task/repository/memory/Task.repository";
+import { TasksRepositoryInMemory } from "../../../infra/task/repository/memory/Task.repository";
 import { CreateTaskUseCase } from "./CreateTaskUseCase";
 
 import { newTask } from "../../../../__tests__/mocks/tasks";
--- src/useCases/task/findAll/GetAllTasks.integration.spec.ts
+++ src/useCases/task/findAll/GetAllTasks.integration.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 
-import { TasksRepositoryInMemory } from "@infra/task/repository/memory/Task.repository";
+import { TasksRepositoryInMemory } from "../../../infra/task/repository/memory/Task.repository";
 import { GetAllTasksUseCase } from "./GetAllTasksUseCase";
 
 import { tasks } from "../../../../__tests__/mocks/tasks";
