225ba47bbd8e30fe90479f2092a6fc805c16e3a1
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-04 10:44:43 -0300

    test(infra/task/controller): remove authorization tests
    - update fake token

--- src/infra/api/http/middleware/auth/index.ts
+++ src/infra/api/http/middleware/auth/index.ts
@@ -5,7 +5,7 @@ class AuthMiddleware {
   constructor(private authService?: IAuthService) {}
 
   handle = async (req: Request, _res: Response, next: NextFunction) => {
-    const fakeUser = "24d5ad6d-b864-4dee-a217-2608b6706cb6"
+    const fakeUser = "24d5ad6d-b864-4dee-a217-2608b6706cb6";
 
     req.body = { ...req.body, userId: fakeUser };
     next();
